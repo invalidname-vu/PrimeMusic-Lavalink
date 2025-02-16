b025b2d8e27b785d9b6dbc1470694418e8f1943d
invalidname-vu <68509631+invalidname-vu@users.noreply.github.com>
2025-02-16 11:45:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 module.exports = {
   TOKEN: "",
   language: "en",
-  ownerID: ["1095540388594466816", ""], 
+  ownerID: ["1004206704994566164", ""], 
   mongodbUri : "mongodb+srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&w=majority",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
